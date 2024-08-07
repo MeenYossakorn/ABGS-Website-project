13401cd95b8e438435421692525fc2a68c9dc122
MeenYossakorn <143907645+MeenYossakorn@users.noreply.github.com>
2024-08-08 00:18:22 +0700

    First commit
    First commit

---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Content from "./components/Content/Content";
+
+export default function Main() {
+  return (
+    <>
+      <Navbar />
+
+      <Content />
+
+      <Footer />
+    </>
+  );
+}
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,48 @@
+export default function Navbar() {
+  return (
+    //Navbar space
+    <div className="navbar bg-blue">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-white text-xl">
+          AUTOMATIC BARRIER GATE SYSTEM
+        </a>
+      </div>
+
+      <div className="flex-none">
+        <a className="btn btn-ghost text-white text-xl">SOMRAK JAIDEE</a>
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar flex items-center space-x-4 p-2"
+          >
+            <div className="w-10 rounded-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            </div>
+          </div>
+
+          <ul
+            tabIndex={0}
+            className="menu menu-lg dropdown-content bg-base-100 rounded-box z-[1] mt-1 w-60 p-2 shadow"
+          >
+            <li>
+              <a class="flex justify-center">MAIN</a>
+            </li>
+
+            <li>
+              <a class="flex justify-center">MY PROFILE</a>
+            </li>
+
+            <li>
+              <a class="flex justify-center">MY CAR</a>
+            </li>
+
+            <li>
+              <a class="flex justify-center">LOGOUT</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
