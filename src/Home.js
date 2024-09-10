3d0c7cb4367b604cc1462322afe378f9a169c835
MeenYossakorn <143907645+MeenYossakorn@users.noreply.github.com>
2024-09-10 22:33:20 +0700

    Edit and sort folder
    Edit and sort folder

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,18 @@
-import Content from "./components/Content/Content";
-import Navbar from "./components/Navbar/Navbar.js";
-import Footer from "./components/Footer/Footer.js";
+import Content from "./components/UserMainPage/Content.js";
+import Navbar from "./components/UserMainPage/Navbar/Navbar.js";
+import Footer from "./components/UserMainPage/Footer/Footer.js";
 import { auth } from "./Auth/firebase.js";
 
-
-
 export default function Home() {
-
   return (
     <>
-     <div>
-      {auth.currentUser.email ? (
-        <p>Email: {auth.currentUser.email}</p>
-      ) : (
-        <p>No user is logged in</p>
-      )}
-    </div>
+      <div>
+        {auth.currentUser.email ? (
+          <p>Email: {auth.currentUser.email}</p>
+        ) : (
+          <p>No user is logged in</p>
+        )}
+      </div>
       <Navbar />
       <Content />
       <Footer />
