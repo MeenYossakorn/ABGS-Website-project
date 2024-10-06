25e414030f2e8d6162f89191130d8144f055e343
dew1213 <dew64527@gmail.com>
2024-10-06 20:37:40 +0700

    2nd commit

---
+++ client/src/Home.js
@@ -0,0 +1,26 @@
+import Content from "./UserMainPage/Content.js";
+import Navbar from "./UserMainPage/Navbar/Navbar.js";
+import Footer from "./UserMainPage/Footer/Footer.js";
+import { auth } from "./Auth/firebase.js";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./Auth/index.js";
+
+export default function Home() {
+  const navigate = useNavigate();
+const {user} = useAuth();
+
+  return (
+    <>
+      <div>
+      {user ? (
+        <p>Email: {user.email}</p>
+      ) : (
+        <p>No user is logged in</p>
+      )}
+    </div>
+      <Navbar />
+      <Content />
+      <Footer />
+    </>
+  );
+}
--- server/index.js
+++ server/index.js
@@ -1,40 +1,77 @@
 const express = require("express")
 const bodyparser = require("body-parser")
-const app = express()
+
+
+var admin = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 const { getStorage } = require("firebase/storage");
-
-app.use(bodyparser.json())
-
 const port = 8000
+const app = express()
+const db = admin.firestore()
+
+app.use(express.json())
 
 let users=[]
 
-//Path ไปที่ตรงไหน
-app.get("/test",(req,res)=>{
- let user = {
-  firstname: "test",
-  lastname:"eiei",
-  age:"14"
- }
-  res.json(user)
-})
 
-//path = GET /users
-app.get("/users",(req,res)=>{
-  console.log("test")
-  res.json(users)
-})
 
-// path = POST /user
-app.post("/user",(req,res)=>{
-  let user = req.body
-  users.push(user)
-  res.json({
-    message : "add ok",
-    user:user
-  })
-})
+app.post("/users", async (req, res) => {
+  try {
+    console.log(req.body);
+    const [day, month, year] = req.body.birthday.split("/");
+    const birthdayDate = new Date(`${year}-${month}-${day}`);
+
+    await db
+      .collection("users")
+      .doc("/" + req.body.uid + "/")
+      .create({
+        uid: req.body.uid,
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        birthday: birthdayDate,
+        tel: req.body.tel,
+        role: "customer",
+      });
+
+    return res.status(200).send({ status: "success", message: "data saved" });
+  } catch (error) {
+    console.log(error.message);
+    console.log("error");
+    return res.status(500).send({ status: "failed" });
+  }
+});
+
+
+// //Path ไปที่ตรงไหน
+// app.get("/users",(req,res)=>{
+//  let user = {
+//   firstname: "test",
+//   lastname:"eiei",
+//   age:"14"
+//  }
+//   res.json(user)
+// })
+
+// //path = GET /users
+// app.get("/users",(req,res)=>{
+//   console.log("test")
+//   res.json(users)
+// })
+
+// // path = POST /user
+// app.post("/user",(req,res)=>{
+//   let user = req.body
+//   users.push(user)
+//   res.json({
+//     message : "add ok",
+//     user:user
+//   })
+// })
 
 app.listen(port,(req,res)=>{
 
